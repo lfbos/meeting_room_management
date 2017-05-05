acb8a092d968f179167bcac0c035d502da0bab4c
Luis Boscan <luife642@gmail.com>
2017-05-05 19:31:58 -0300

    main app settings changed

--- meetings_management/apps.py
+++ meetings_management/apps.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class MeetingsManagementConfig(AppConfig):
     name = 'meetings_management'
+    label = 'meetings_management'
+    verbose_name = _('Meetings Management')
