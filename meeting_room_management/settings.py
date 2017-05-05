a4cc4379b49725ba060b315dd9b7545b7f91c803
Luis Boscan <luife642@gmail.com>
2017-05-05 19:26:40 -0300

    LOCALE path added

--- meeting_room_management/settings.py
+++ meeting_room_management/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOCALE_PATHS = ('locale',)
