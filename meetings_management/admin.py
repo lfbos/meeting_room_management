e928f922a10e7e261683810975ea6c09a4535dff
Luis Boscan <luife642@gmail.com>
2017-05-05 19:30:30 -0300

    MeetingRoom model admin added

--- meetings_management/admin.py
+++ meetings_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import BaseModelAdmin
+from meetings_management.models import MeetingRoom
+
+admin.site.register(MeetingRoom, BaseModelAdmin)
