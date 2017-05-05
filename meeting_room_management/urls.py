21c1ed39cfd8c9f1fcb8830a7c2376bc75372b13
Luis Boscan <luife642@gmail.com>
2017-05-05 19:39:25 -0300

    Admin site titles changed

--- meeting_room_management/urls.py
+++ meeting_room_management/urls.py
@@ -1,20 +1,11 @@
-"""meeting_room_management URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+# coding=utf-8
 from django.conf.urls import url
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+admin.site.site_title = 'Gestión para Salas de Reuniones'
+admin.site.site_header = 'Gestión para Salas de Reuniones'
+admin.site.index_title = 'Módulos'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
