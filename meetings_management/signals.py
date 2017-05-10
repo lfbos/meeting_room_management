f43ad912116c54d43e0197907621dca65c3a83cf
Luis Boscan <luife642@gmail.com>
2017-05-10 18:38:24 -0300

    Signal fixed to check meeting room reservation availability

--- meetings_management/signals.py
+++ meetings_management/signals.py
@@ -1,13 +1,33 @@
-from django.db.models.signals import pre_save
+from django.db.models import Q
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from meetings_management.exceptions import ReservationCollisionException
+from meetings_management.exceptions import (
+    ReservationCollisionException,
+    ReservationReservedCollisionException
+)
 from meetings_management.models import MeetingRoomReservation
 
 
-@receiver(pre_save, sender=MeetingRoomReservation)
-def check_meeting_room_availability(sender, instance, raw, **kwargs):
-    if not MeetingRoomReservation.objects.filter(
-            reserved_from__gte=instance.reserved_from,
-            reserved_until__lte=instance.reserved_until):
-        raise ReservationCollisionException('Error! Reservations already exist at that time')
+@receiver(post_save, sender=MeetingRoomReservation)
+def check_meeting_room_availability(sender, instance, created, raw, **kwargs):
+    if created and not raw:
+        q1 = Q(reserved_from__range=(instance.reserved_from, instance.reserved_until))
+        q2 = Q(reserved_until__range=(instance.reserved_from, instance.reserved_until))
+
+        reservations = MeetingRoomReservation.objects.filter(q1 | q2) \
+            .filter(meeting_room=instance.meeting_room) \
+            .exclude(pk=instance.pk)
+
+        reserved = reservations.filter(confirmed=False)
+        confirmed = reservations.filter(confirmed=True)
+
+        if confirmed.count() > 0:
+            MeetingRoomReservation.objects.filter(pk=instance.pk).delete()
+            raise ReservationCollisionException('Error! Reservations already confirmed at that time')
+        elif reserved.count() > 0:
+            MeetingRoomReservation.objects.filter(pk=instance.pk).delete()
+            raise ReservationReservedCollisionException(
+                'Reservations exist at that time, contact with the owner if '
+                'you need urgent the room'
+            )
