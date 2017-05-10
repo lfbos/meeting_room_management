4e3da03346731db2099770676883d4af3d32db7d
Luis Boscan <luife642@gmail.com>
2017-05-10 18:37:23 -0300

    Reservation & request model added

--- meetings_management/admin.py
+++ meetings_management/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
 from core.admin import BaseModelAdmin
-from meetings_management.models import MeetingRoom, MeetingRoomUser
+from meetings_management.models import (
+    MeetingRoom,
+    MeetingRoomUser,
+    MeetingRoomReservation,
+    MeetingRoomRequest
+)
 
 admin.site.register(MeetingRoom, BaseModelAdmin)
 admin.site.register(MeetingRoomUser, BaseModelAdmin)
+admin.site.register(MeetingRoomReservation, BaseModelAdmin)
+admin.site.register(MeetingRoomRequest, BaseModelAdmin)
