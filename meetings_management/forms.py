c66fee2738278e223ad889225539a7ac76941029
Luis Boscan <luife642@gmail.com>
2017-05-10 18:41:28 -0300

    new forms added to create reservations & requests

--- meetings_management/forms.py
+++ meetings_management/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
+from meetings_management.models import MeetingRoom, MeetingRoomUser, MeetingRoomReservation
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
@@ -35,20 +37,21 @@ class NewRoomForm(forms.Form):
     ))
 
     capacity = forms.CharField(widget=forms.NumberInput(
-        attrs={'class': 'form-control'}
+        attrs={
+            'class': 'form-control',
+            'min': 1
+        }
     ))
 
     available_from = forms.CharField(widget=forms.DateTimeInput(
         attrs={
-            'class': 'form-control',
-            'id': 'available_from'
+            'class': 'form-control'
         }
     ))
 
     available_until = forms.CharField(widget=forms.DateTimeInput(
         attrs={
-            'class': 'form-control',
-            'id': 'available_until'
+            'class': 'form-control'
         }
     ))
 
@@ -59,3 +62,107 @@ class NewRoomForm(forms.Form):
             'data-role': 'tagsinput'
         }
     ))
+
+
+class NewReservationForm(forms.Form):
+    meeting_room = forms.ModelChoiceField(
+        queryset=MeetingRoom.objects.all(),
+        widget=forms.Select(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    reserved_from = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+
+    reserved_until = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+
+    amount = forms.CharField(
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-control',
+                'min': 1
+            }
+        )
+    )
+
+    supplies = forms.CharField(
+        widget=forms.SelectMultiple(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+
+    confirmed = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput()
+    )
+
+
+class NewRequestForm(forms.Form):
+    to_user = forms.ModelChoiceField(
+        queryset=MeetingRoomUser.objects.filter(role=0),
+        widget=forms.Select(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    reservation = forms.ModelChoiceField(
+        queryset=MeetingRoomReservation.objects.filter(confirmed=False),
+        widget=forms.Select(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    checked = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput()
+    )
+
+    reserved_from = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    reserved_until = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={'class': 'form-control'}
+        )
+    )
+
+    amount = forms.CharField(
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-control',
+                'min': 1
+            }
+        )
+    )
+
+    supplies = forms.CharField(
+        widget=forms.SelectMultiple(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+
+    message = forms.CharField(
+        max_length=128,
+        widget=forms.Textarea(
+            attrs={'class': 'form-control'}
+        )
+    )
